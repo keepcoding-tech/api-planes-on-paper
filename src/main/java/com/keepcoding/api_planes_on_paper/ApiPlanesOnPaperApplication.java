22a5c61d2716a97ce4c4c77ea4a44c863a5ff182
Daniel Tanase <tanasedaniel54@gmail.com>
2021-05-17 19:56:21 +0300

    migrating to maven from gradle

---
+++ src/main/java/com/keepcoding/api_planes_on_paper/ApiPlanesOnPaperApplication.java
@@ -0,0 +1,11 @@
+package com.keepcoding.api_planes_on_paper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlanesOnPaperApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlanesOnPaperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/keepcoding/api_planes_on_paper/ApiPlanesOnPaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keepcoding.api_planes_on_paper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlanesOnPaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
